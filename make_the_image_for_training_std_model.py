c9ccc9e52106e9f1fa8f12ec2dc43bd7e2147112
TarunSharmaNt <tarun.sharma@numbertheory.ai>
2021-03-26 15:43:33 +0530

    final commit

--- make_the_image_for_training_std_model.py
+++ make_the_image_for_training_std_model.py
@@ -2,25 +2,55 @@
 ##########################Dated March 26#####################################
 
 
+#################################
+import os
+print(os.getpid())
+
+
+import socket
+print(socket.gethostname())
+#################################
+
 import shutil
 import glob
 import copy
 import pandas as pd
 import numpy as np
 
-
 #importing the cv2
 import cv2
+
+
+
+hostname=socket.gethostname()
+
 source_folder =""
 csv_path=""
-
+dest_folder=""
+if hostname == "tarun-Lenovo-V14-IIL":
+    source_folder =""
+    csv_path=""
+    dest_folder=""
 
 #reading the csv
 df = pd.read_csv("")
 
 
-#test change
+#image_name_list
+image_name_list = df["image_name"].tolist()
 
+for img in glob.glob():
+    image_name = img.split("/")[-1]
+
+
+    if image_name not in image_name_list:
+        continue
+
+
+    #copy for the 
+    shutil.copy(img,dest_folder)
+
+    
 
 
 
